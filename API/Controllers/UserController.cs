e98370c4167510f475715ace4dc170d4345f3187
Amit Verma <amitverma1981@gmail.com>
2021-10-14 15:36:01 +0530

    changes

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,39 +1,38 @@
-﻿using DTOModels;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Models.Configuration;
-using Models.Models;
-using Operations.IOperations;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace API.Controllers
+﻿namespace API.Controllers
 {
+    using DTOModels;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+    using Models.Configuration;
+    using Models.Models;
+    using Operations.IOperations;
+    using System;
+    using System.Threading.Tasks;
+
     [ApiController]
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
-        private readonly ILogger<UserController> _logger;
-        private readonly IUserOperations _userService;
-        private readonly IConfiguration _configuration;
-        public UserController(ILogger<UserController> logger, IUserOperations userService, IConfiguration configuration) : base(logger)
+        private readonly ILogger<UserController> logger;
+        private readonly IUserOperations userService;
+        private readonly IConfiguration configuration;
+        public UserController(ILogger<UserController> logger, IUserOperations userService, IConfiguration configuration) 
+            : base(logger)
         {
-            _logger = logger;
-            _userService = userService;
-            _configuration = configuration;
+            this.logger = logger;
+            this.userService = userService;
+            this.configuration = configuration;
         }
 
         [AllowAnonymous]
         [HttpPost("authenticate")]
         public async Task<ApiResponse> Authenticate([FromBody] UserDTO userParam)
         {
-            var appSettingsSection = _configuration.GetSection("JWT");
+            var appSettingsSection = this.configuration.GetSection("JWT");
             var appSettings = appSettingsSection.Get<JWT>();
-            var user = await _userService.Authenticate(userParam.UserName, userParam.Password, appSettings);
+            var user = await this.userService.Authenticate(userParam.UserName, userParam.Password, appSettings);
 
             if (user == null)
                 return CreateResponse(new UnauthorizedAccessException("Username or password is incorrect"));
@@ -45,7 +44,7 @@ namespace API.Controllers
         [HttpPost("adduser")]
         public async Task<ApiResponse> AddUser([FromBody] UserDTO userParam)
         {
-            var user = await _userService.AddUser(userParam.UserName, userParam.Password);
+            var user = await this.userService.AddUser(userParam.UserName, userParam.Password);
 
             if (user == null)
                 return CreateResponse(new UnauthorizedAccessException("Username not created"));
@@ -57,7 +56,7 @@ namespace API.Controllers
         [HttpPost("adduserrole")]
         public async Task<ApiResponse> AddUserRole(Role roleParam)
         {
-            var role = await _userService.AddRole(roleParam.Name);
+            var role = await this.userService.AddRole(roleParam.Name);
 
             if (!role)
                 return CreateResponse(new UnauthorizedAccessException("Username role not created or already exists"));
